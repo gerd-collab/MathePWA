711bbaed8bb5c8fe8d1a4b745942704b69474665
gerd-collab <gerd.wenning@gmail.com>
2024-08-19 15:53:21 +0200

    überschrift weg

--- app.js
+++ app.js
@@ -1,53 +1,50 @@
 const content = {
-    'klasse2': {
-      'title': 'Klasse 2',
-      'topics': [
-        'Teilbarkeit',
-        'Bruchrechnen',
-        'Ganze Zahlen',
-        'Geometrische Grundlagen',
-        'Proportionalitäten',
-        'Dreiecke'
-      ]
-    },
-    'klasse3': {
-      'title': 'Klasse 3',
-      'topics': [
-        'Gleichungen',
-        'Vierecke',
-        'Prozentrechnen',
-        'Daten',
-        'Lernen mit Technologien'
-      ]
-    }
-  };
-  
-  function createNavigation() {
-    const nav = document.getElementById('main-nav');
-    for (const [key, value] of Object.entries(content)) {
-      const button = document.createElement('button');
-      button.textContent = value.title;
-      button.addEventListener('click', () => loadContent(key));
-      nav.appendChild(button);
-    }
+  'klasse2': {
+    'title': 'Klasse 2',
+    'topics': [
+      'Teilbarkeit',
+      'Bruchrechnen',
+      'Ganze Zahlen',
+      'Geometrische Grundlagen',
+      'Proportionalitäten',
+      'Dreiecke'
+    ]
+  },
+  'klasse3': {
+    'title': 'Klasse 3',
+    'topics': [
+      'Gleichungen',
+      'Vierecke',
+      'Prozentrechnen',
+      'Daten',
+      'Lernen mit Technologien'
+    ]
   }
-  
-  function loadContent(key) {
-    const main = document.getElementById('content');
-    main.innerHTML = '';
-    const h2 = document.createElement('h2');
-    h2.textContent = content[key].title;
-    main.appendChild(h2);
-    const ul = document.createElement('ul');
-    content[key].topics.forEach(topic => {
-      const li = document.createElement('li');
-      li.textContent = topic;
-      ul.appendChild(li);
-    });
-    main.appendChild(ul);
+};
+
+function createNavigation() {
+  const nav = document.getElementById('main-nav');
+  for (const [key, value] of Object.entries(content)) {
+    const button = document.createElement('button');
+    button.textContent = value.title;
+    button.addEventListener('click', () => loadContent(key));
+    nav.appendChild(button);
   }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    createNavigation();
-    loadContent('klasse2');
+}
+
+function loadContent(key) {
+  const main = document.getElementById('content');
+  main.innerHTML = '';
+  const ul = document.createElement('ul');
+  content[key].topics.forEach(topic => {
+    const li = document.createElement('li');
+    li.textContent = topic;
+    ul.appendChild(li);
   });
+  main.appendChild(ul);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  createNavigation();
+  loadContent('klasse2');
+});
